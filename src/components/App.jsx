b34704ec8c51f429b512b665514b3771daa7b55b
SyrdiiDima <dimasyrdij@icloud.com>
2022-07-14 00:28:44 +0300

    start project

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,66 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from "react";
+import Form from "./Form/Form";
+import FilterContact from "./FilterContact/FilterContact";
+import ContactList from "./ContactList/ContactList";
+
+export class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  handleAddContact = contactItem => {
+    this.setState(prevState => ({
+      contacts: [contactItem,...prevState.contacts ],
+    }));
+  };
+
+
+  handleDeleteContact = contactId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact=> contact.id !== contactId),
+    }));
+  };
+
+  changeFilter = event => {
+    this.setState({ filter: event.currentTarget.value });
+  };
+
+  getVisibleContacts = () => {
+    const normalizedFilter = this.state.filter.toLowerCase();
+
+    return this.state.contacts.filter(({ name }) =>
+      name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+  render() {
+    const { contacts, filter } = this.state;
+    const visibleContacts = this.getVisibleContacts();
+    const contactsName = contacts.map(contact => contact.name);
+
+    return (
+      <div>
+        <h1 >Phonebook</h1>
+        <Form
+          onSubmit={this.handleAddContact}
+          contactsName={contactsName}
+        />
+
+        <h2 >Contacts</h2>
+        <div>
+          <FilterContact value={filter} onChange={this.changeFilter} />
+          <ContactList
+            visibleContacts={visibleContacts}
+            onDeleteContact={this.handleDeleteContact}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
